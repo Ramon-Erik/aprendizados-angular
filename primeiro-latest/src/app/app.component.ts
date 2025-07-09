bc92f2469125be74dac627d1f14fbcd5ad23f247
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-07-09 15:14:01 -0300

    meia aula de variaveis

--- primeiro-latest/src/app/app.component.ts
+++ primeiro-latest/src/app/app.component.ts
@@ -3,18 +3,19 @@ import { RouterOutlet } from '@angular/router';
 
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplatesBindingComponent } from "./components/templates/templates-binding/templates-binding.component";
+import { TemplatesVariablesComponent } from "./components/templates/templates-variables/templates-variables.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NewComponent, TemplatesBindingComponent],
+  imports: [NewComponent, TemplatesBindingComponent, TemplatesVariablesComponent],
   template: `
   <!--<router-outlet></router-outlet> -->
     <h1>Oii {{title}}</h1>
     <div class="theme-dark">
       <!-- <app-new-component/>
       <app-templates-binding/> -->
-      
+      <app-templates-variables/>
     </div>
   `,
 })
