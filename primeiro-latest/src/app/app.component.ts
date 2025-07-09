7a11200496f3b264bd776b647df09ffe124bbd73
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-07-09 14:45:32 -0300

    interpolação atributos

--- primeiro-latest/src/app/app.component.ts
+++ primeiro-latest/src/app/app.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { NewComponent } from './components/new-component/new-component.component';
+import { TemplatesBindingComponent } from "./components/templates-binding/templates-binding.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NewComponent],
+  imports: [NewComponent, TemplatesBindingComponent],
   template: `
   <!--<router-outlet></router-outlet> -->
     <h1>Oii {{title}}</h1>
     <div class="theme-dark">
       <app-new-component/>
+      <app-templates-binding/>
     </div>
   `,
 })
--- primeiro-latest/src/app/components/templates-binding/templates-binding.component.ts
+++ primeiro-latest/src/app/components/templates-binding/templates-binding.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-templates-binding',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './templates-binding.component.html',
   styleUrl: './templates-binding.component.scss'
 })
 export class TemplatesBindingComponent {
+  public name: string = 'Ramon'
+  public age: number = 19
+  public isDisabled: boolean = true
+  public condition = this.name ==="Ramon" ? "som" : "asa" 
+  public isGreen = this.name === "Ramon" ? "green" : "blue" 
 
+  public older() {
+    return this.age++
+  }
+
+  public newer() {
+    return this.age--
+  }
+  public amarelar() {
+    return this.isGreen = "yellow"
+  }
 }
