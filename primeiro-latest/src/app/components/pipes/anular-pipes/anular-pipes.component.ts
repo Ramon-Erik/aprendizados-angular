1a1f4e596d921442d54b19defd59bb4ae71268c5
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-07-25 00:13:23 -0300

    outros pipes comuns

--- primeiro-latest/src/app/components/pipes/anular-pipes/anular-pipes.component.ts
+++ primeiro-latest/src/app/components/pipes/anular-pipes/anular-pipes.component.ts
@@ -1,19 +1,25 @@
-import { AsyncPipe, DatePipe } from '@angular/common';
+import {
+  AsyncPipe,
+  CurrencyPipe,
+  DatePipe,
+  DecimalPipe,
+  PercentPipe,
+} from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { delay, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-anular-pipes',
   standalone: true,
-  imports: [DatePipe, AsyncPipe],
+  imports: [DatePipe, AsyncPipe, CurrencyPipe, DecimalPipe, PercentPipe],
   templateUrl: './anular-pipes.component.html',
-  styleUrl: './anular-pipes.component.scss'
+  styleUrl: './anular-pipes.component.scss',
 })
 export class AnularPipesComponent {
-  public date = signal<Date>(new Date())
+  public date = signal<Date>(new Date());
   public loadingData$: Observable<string[]> = of([
-      'Confissões',
-      'Ortodoxia',
-      'Cartas de um aprendiz ao seu diabo',
-    ]).pipe(delay(3000));
+    'Confissões',
+    'Ortodoxia',
+    'Cartas de um aprendiz ao seu diabo',
+  ]).pipe(delay(3000));
 }
