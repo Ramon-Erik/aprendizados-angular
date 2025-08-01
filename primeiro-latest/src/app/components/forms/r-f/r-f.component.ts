2edb72f31e3cada759ff9851e1e1e6a6331bdb04
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-08-01 15:34:17 -0300

    usando form builder

--- primeiro-latest/src/app/components/forms/r-f/r-f.component.ts
+++ primeiro-latest/src/app/components/forms/r-f/r-f.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-r-f',
@@ -9,17 +9,31 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './r-f.component.scss'
 })
 export class RFComponent {
+  constructor(
+    private _fb: FormBuilder //usa isso opara evitar a #FORMA 1 e usar a #FORMA 2
+  ) {}
+  // tem como fazer isso sem construtor:
+  #fb2 = inject(FormBuilder)
   name = new FormControl('Ramon') 
   public updateName() {
     this.name.setValue('cladio')
   }
-
+  // #FORMA 1 - com new em tudo
   public profileForm = new FormGroup({
     data: new FormControl(''),
     devoc: new FormGroup({
       cristo: new FormControl('Sagrado Coração'),
       mariana: new FormControl('NS Carmo')
     }),
+  })
+  // #FORMA 2
+  public profileForm2 = this._fb.group({
+    data: [''],
+    devoc: this._fb.group({
+      cristo: ['Sagrado Coração'],
+      mariana: ['NS Carmo']
+    }),
+    favoriteFoods: this._fb.array([['lasagna']])
   })
   public clearForm() {
     this.profileForm.patchValue({
