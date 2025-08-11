18975093aa4d16289c3e3ef2322bc74f28c70d95
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-08-11 17:33:47 -0300

    rotas inexistentes

--- primeiro-latest/src/app/app.routes.ts
+++ primeiro-latest/src/app/app.routes.ts
@@ -2,8 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ServicesComponent } from './pages/services/services.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
+  {
+    path: '**',
+    title: 'Home',
+    component: NotFoundComponent
+  },
   {
     path: '',
     title: 'Home',
