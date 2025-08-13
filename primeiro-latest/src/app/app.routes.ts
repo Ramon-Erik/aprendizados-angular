94acd27764759754e8d81bb30e6e30bddb5002e4
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-08-13 15:17:52 -0300

    prittier

--- primeiro-latest/src/app/app.routes.ts
+++ primeiro-latest/src/app/app.routes.ts
@@ -9,29 +9,39 @@ export const routes: Routes = [
   {
     path: '',
     title: 'rota padrão',
-    loadComponent: () => import('./pages/home/home.component').then(p => p.HomeComponent),
+    loadComponent: () =>
+      import('./pages/home/home.component').then((p) => p.HomeComponent),
     //children: [ // tem como colar essas filhas em outro arquivo, para fazer um lazy loading maior
     //],
   },
-    {
-      path: 'home',
-      title: 'Home',
-      loadComponent: () => import('./pages/home/home.component').then(p => p.HomeComponent),
-    },
-    {
-      path: 'about',
-      title: 'Sobre nós',
-      loadComponent: () => import('./pages/about/about.component').then(p => p.AboutComponent),
-    },
-    {
-      path: 'services/:id',
-      title: 'Nossos serviços',
-      loadComponent: () => import('./pages/services/services.component').then(p => p.ServicesComponent),
-      canActivate: [canActGuard] // a proteção de rotas serve sempre individualmente
-    },
-  { // rotas coringas ficam sempre no final
+  {
+    path: 'home',
+    title: 'Home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((p) => p.HomeComponent),
+  },
+  {
+    path: 'about',
+    title: 'Sobre nós',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((p) => p.AboutComponent),
+  },
+  {
+    path: 'services/:id',
+    title: 'Nossos serviços',
+    loadComponent: () =>
+      import('./pages/services/services.component').then(
+        (p) => p.ServicesComponent
+      ),
+    canActivate: [canActGuard], // a proteção de rotas serve sempre individualmente
+  },
+  {
+    // rotas coringas ficam sempre no final
     path: '**',
     title: '404',
-    loadComponent: () => import('./pages/not-found/not-found.component').then(p => p.NotFoundComponent),
-  }
+    loadComponent: () =>
+      import('./pages/not-found/not-found.component').then(
+        (p) => p.NotFoundComponent
+      ),
+  },
 ];
